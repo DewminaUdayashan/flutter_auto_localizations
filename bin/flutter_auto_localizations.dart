752d2e5118f9cc20afbcafdcab6b14cff9441fd2
Dewmina Udayashan <dewminaudayashan@gmail.com>
2025-02-06 16:08:48 +0530

    Initialize flutter_auto_localizations

---
+++ bin/flutter_auto_localizations.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_auto_localizations/flutter_auto_localizations.dart' as flutter_auto_localizations;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_auto_localizations.calculate()}!');
+}
---
+++ test/flutter_auto_localizations_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_auto_localizations/flutter_auto_localizations.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
